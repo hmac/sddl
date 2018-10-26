d68c222ae2f6b177144ce789754ca648c9460df1
Harry Maclean <harrymaclean@gocardless.com>
2018-10-26 22:45:30 +0100

    use record syntax

--- src/ToSql.hs
+++ src/ToSql.hs
@@ -1,9 +1,11 @@
-{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE NamedFieldPuns        #-}
 
 module ToSql where
 
 import           Core
 import           Data.List (intercalate, intersperse)
+import           Prelude   hiding (null)
 
 -- Utilities
 paren :: String -> String
@@ -32,58 +34,59 @@ instance ToSql SqlType where
   toSql SJson      = "jsonb"
 
 instance ToSql ColDef where
-  toSql (ColDef s t n) = let isNull = if n then "NULL" else "NOT NULL"
-                          in unwords [s, toSql t, isNull]
+  toSql ColDef { name, type_, null } =
+    let isNull = if null then "NULL" else "NOT NULL"
+    in unwords [name, toSql type_, isNull]
 
 instance ToSql Statement where
-  toSql (AddColumn tbl col colType) =
-    unwords ["ALTER TABLE", tbl, "ADD COLUMN", col, toSql colType]
-  toSql (DropColumn tbl col) =
-    unwords ["ALTER TABLE", tbl, "DROP COLUMN", col]
-  toSql (CreateTable name p cols) =
+  toSql AddColumn { table, column, type_ } =
+    unwords ["ALTER TABLE", table, "ADD COLUMN", column, toSql type_]
+  toSql DropColumn { table, column } =
+    unwords ["ALTER TABLE", table, "DROP COLUMN", column]
+  toSql CreateTable { table, prefix, definition } =
     let idCol =
           "id character varying(32) DEFAULT " ++
-          "public.gen_gc_id('" ++ p ++ "'::text, '" ++ name ++ "_id_seq'::text) NOT NULL"
-        createSequence = "CREATE SEQUENCE public." ++ name ++ "_id_seq " ++
+          "public.gen_gc_id('" ++ prefix ++ "'::text, '" ++ table ++ "_id_seq'::text) NOT NULL"
+        createSequence = "CREATE SEQUENCE public." ++ table ++ "_id_seq " ++
                      "START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;"
-        colDefs = map (++ ",") $ idCol : map toSql cols
-        createTable = multiline $ wrap ("CREATE TABLE " ++ name ++ " (") ")" colDefs
+        colDefs = map (++ ",") $ idCol : map toSql definition
+        createTable = multiline $ wrap ("CREATE TABLE " ++ table ++ " (") ")" colDefs
     in
       multiline [createSequence, createTable]
-  toSql (MakeColumnNotNull table col) =
-    unwords ["ALTER TABLE ONLY", table, "ALTER COLUMN", col, "SET NOT NULL"]
-  toSql (MakeColumnNull table col) =
-    unwords ["ALTER TABLE ONLY", table, "ALTER COLUMN", col, "SET NULL"]
-  toSql (ChangeColumnDefault table col _ to) =
-    unwords ["ALTER TABLE ONLY", table, "ALTER COLUMN", col, "SET DEFAULT", quote to]
-  toSql (DropColumnDefault table col _) =
-    unwords ["ALTER TABLE ONLY", table, "ALTER COLUMN", col, "DROP DEFAULT"]
-  toSql (CreateIndex table cols name _mwhere) =
+  toSql MakeColumnNotNull { table, column } =
+    unwords ["ALTER TABLE ONLY", table, "ALTER COLUMN", column, "SET NOT NULL"]
+  toSql MakeColumnNull { table, column } =
+    unwords ["ALTER TABLE ONLY", table, "ALTER COLUMN", column, "SET NULL"]
+  toSql ChangeColumnDefault { table, column, to } =
+    unwords ["ALTER TABLE ONLY", table, "ALTER COLUMN", column, "SET DEFAULT", quote to]
+  toSql DropColumnDefault { table, column } =
+    unwords ["ALTER TABLE ONLY", table, "ALTER COLUMN", column, "DROP DEFAULT"]
+  toSql CreateIndex { table, columns, name } =
     unwords
       [ "CREATE INDEX CONCURRENTLY"
       , name
       , "ON"
       , table
       , "USING btree"
-      , (paren . list) cols
+      , (paren . list) columns
       ]
-  toSql (DropIndex name) = unwords ["DROP INDEX CONCURRENTLY", name]
-  toSql (DropTable name) = unwords ["DROP TABLE", name]
-  toSql (AddForeignKey srcTable srcCol tarTable tarCol name) =
+  toSql DropIndex { name } = unwords ["DROP INDEX CONCURRENTLY", name]
+  toSql DropTable { table } = unwords ["DROP TABLE", table]
+  toSql AddForeignKey { sourceTable, sourceColumn, targetTable, targetColumn, name } =
     unwords
       [ "ALTER TABLE ONLY"
-      , srcTable
+      , sourceTable
       , "ADD CONSTRAINT"
       , name
       , "FOREIGN KEY"
-      , paren srcCol
+      , paren sourceColumn
       , "REFERENCES"
-      , tarTable ++ paren tarCol
+      , targetTable ++ paren targetColumn
       ]
-  toSql (DropConstraint name) = unwords ["DROP CONSTRAINT", name]
+  toSql DropConstraint { name } = unwords ["DROP CONSTRAINT", name]
 
 instance ToSql Migration where
-  toSql (Migration statements lockTimeout statementTimeout transaction) =
+  toSql Migration { statements, lockTimeout, statementTimeout, transaction } =
     let lt = "SET lock_timeout = " ++ show lockTimeout
         st = "SET statement_timeout = " ++ show statementTimeout
         commands = if transaction
