1323f4606bf43e2101942f729236eaf9f0ca4fb8
Harry Maclean <harrymaclean@gocardless.com>
2018-10-26 21:58:48 +0100

    Add --reverse flag for reverse migration generation

--- app/Main.hs
+++ app/Main.hs
@@ -2,16 +2,18 @@
 
 module Main where
 
-import           Core            (Migration (..))
-import qualified Data.ByteString as B (getContents)
-import qualified Data.Yaml       as Yaml
+import           Core                (Migration (..))
+import qualified Data.ByteString     as B (getContents)
+import qualified Data.Yaml           as Yaml
+import           Options.Applicative
 import qualified Reverse
 import           System.Exit
-import           ToSql           (toSql)
+import           ToSql               (toSql)
 import           Validator
 
 main :: IO ()
 main = do
+  config <- execParser parseConfig
   input <- B.getContents
   statements <- Yaml.decodeThrow input
   let lockTimeout = calculateLockTimeout statements
@@ -19,9 +21,17 @@ main = do
       transaction = determineTransaction statements
       migration = Migration statements lockTimeout statementTimeout transaction
   case validate migration of
-    []   -> do
-      putStrLn (toSql migration)
-      case Reverse.reverse migration of
-        Nothing -> putStrLn "ERROR: cannot reverse migration"
-        Just m  -> putStrLn (toSql m)
+    []   ->
+      case mode config of
+        Forward -> putStrLn (toSql migration)
+        Reverse -> case Reverse.reverse migration of
+                     Nothing -> putStrLn "ERROR: cannot reverse migration"
+                     Just m  -> putStrLn (toSql m)
     errs -> putStrLn ("ERROR: " ++ unlines errs) >> exitWith (ExitFailure 1)
+
+newtype Config = Config { mode :: Mode } deriving (Eq, Show)
+data Mode = Forward | Reverse deriving (Eq, Show)
+
+parseConfig :: ParserInfo Config
+parseConfig = info parser $ fullDesc <> progDesc "sddl" <> header "sddl"
+  where parser = Config <$> flag Forward Reverse (long "reverse" <> short 'r' <> help "Generate reverse migration")
